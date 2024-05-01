0fea7c69ecf98a87784915534cdeb6db9149a5f4
BlueMoonJune <45300174+BlueMoonJune@users.noreply.github.com>
2024-05-01 09:48:52 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,14 @@
+http = require 'socket.http'
+local baseURL = "https://https://raw.githubusercontent.com//BlueMoonJune/loveweb/"
+
+function require(path)
+  local url = baseURL..path:gsub("%.", "/")..".lua"
+  local func, err = load(http.request(url))
+  if func then
+    return func()
+  else
+    error("syntax error in " .. path .. ":\n" .. err)
+  end
+end
+
+require("helloworld")
