83d573c70fc6ec00d3d14a9a13e723adbfd32dff
BlueMoonJune <45300174+BlueMoonJune@users.noreply.github.com>
2024-05-02 16:32:51 +0000

    bundler

--- bundle.py
+++ bundle.py
@@ -1,4 +1,6 @@
 import os
+import base64
+import sys
 
 output = ""
 
@@ -7,16 +9,15 @@ def process(path):
     print(path, "is a...")
     if os.path.isfile(path):
         print("file.")
-        output += "### " + path + "\n"
-        with open(path, 'r') as file:
-            for line in file.readlines():
-                output += line
+        output += ("### " + path + " ###\n")
+        with open(path, 'rb') as file:
+            output += str(base64.b64encode(file.read()))[2:-1] + "\n"
     else:
         print("directory.")
         for item in os.listdir(path):
             process(path + "/" + item)
 
-process("src")
+process(sys.argv[1])
 
 with open('output', 'w') as file:
     file.write(output)
